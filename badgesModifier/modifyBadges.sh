fbdaa4a49a74eed2913969cbb4af612c77065b56
SerVB <servbul@yandex.ru>
2020-08-08 15:04:57 +0300

    Add helper options for bash

--- badgesModifier/modifyBadges.sh
+++ badgesModifier/modifyBadges.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 # https://www.apache.org/licenses/LICENSE-2.0.html
 
+set -e # Any command which returns non-zero exit code will cause this shell script to exit immediately
+set -x # Activate debugging to show execution details: all commands will be printed before execution
+
 python3 battleBadgesModifier.py
 
 cd target/gui/flash/atlases
